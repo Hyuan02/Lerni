ac71395235cb8e3acf4c7151646ac40d0a399930
Hyuan Peixoto <hyuanpeixoto@gmail.com>
2017-11-10 22:15:46 -0300

    versao inicial do prototipo

---
+++ js/inicio.js
@@ -0,0 +1,101 @@
+var opcaoInicial1, opcaoInicial2, opcaoInicial3, opcaoInicial4;
+var opcaoCapitulos1,opcaoCapitulos2,opcaoCapitulos3,opcaoCapitulos4,opcaoCapitulosVoltar;
+var opcaoAtividades1,opcaoAtividades2,opcaoAtividades3;
+var controladorPuzzle=0;
+function preload(){
+	spriteCaixa = loadSpriteSheet("imagens/caixa.png",200,200,1); //load nos sprites com dimensoes e quantidade de frames
+	spriteBola = loadSpriteSheet("imagens/bola.png",100,100,1);
+	spriteEsquadro = loadSpriteSheet("imagens/esquadro.png",300,225,1);
+	imgBalaoA = loadImage("imagens/baloes/balao_azul.png");
+	imgBalaoB = loadImage("imagens/baloes/balao_verde.png");
+	imgBalaoC = loadImage("imagens/baloes/balao_vermelho.png");
+	imgBalaoD = loadImage("imagens/baloes/balao_amarelo.png");
+	imgBalaoE = loadImage("imagens/baloes/balao_preto.png");
+}
+
+function setup(){
+	createCanvas(1280,720);
+	interfaceInicial();
+}
+
+
+function draw(){
+	quadroPuzzles(controladorPuzzle);
+}
+
+function interfaceInicial(){
+	removeElements();
+	background(253,245,202);
+	opcaoInicial1 = createButton('INÍCIO');
+	opcaoInicial2 = createButton('NÃO SEI');
+	opcaoInicial3 = createButton('CONFIGURAÇÕES');
+	opcaoInicial4 = createButton('CRÉDITOS');
+	opcaoInicial1.addClass('opcoesInicial');
+	opcaoInicial2.addClass('opcoesInicial');
+	opcaoInicial3.addClass('opcoesInicial');
+	opcaoInicial4.addClass('opcoesInicial');
+	opcaoInicial1.position(900,170);
+	opcaoInicial2.position(900,255);
+	opcaoInicial3.position(900,340);
+	opcaoInicial4.position(900,425);
+	opcaoInicial1.mousePressed(interfaceCapitulos);
+}
+
+function interfaceCapitulos(){
+	removeElements();
+	background(255,255,255);
+	opcaoCapitulos1 = createButton('Capítulo1');
+	opcaoCapitulos2 = createButton('Capítulo2');
+	opcaoCapitulos3 = createButton('Capítulo3');
+	opcaoCapitulos4 = createButton('Capítulo4');
+	opcaoCapitulosVoltar = createButton('Voltar');
+	opcaoCapitulos1.addClass('opcaoCapitulos');
+	opcaoCapitulos2.addClass('opcaoCapitulos');
+	opcaoCapitulos3.addClass('opcaoCapitulos');
+	opcaoCapitulos4.addClass('opcaoCapitulos');
+	opcaoCapitulosVoltar.addClass('opcaoCapitulos');
+	opcaoCapitulos1.position(200,200);
+	opcaoCapitulos2.position(200,300);
+	opcaoCapitulos3.position(800,200);
+	opcaoCapitulos4.position(800,300);
+	opcaoCapitulosVoltar.position(500,500);
+	opcaoCapitulosVoltar.mousePressed(interfaceInicial);
+	opcaoCapitulos1.mousePressed(interfaceCapitulo1);
+}
+
+function interfaceCapitulo1(){
+	removeElements();
+	background(240,240,250);
+	opcaoAtividades1 = createButton('1');
+	opcaoAtividades2 = createButton('2');
+	opcaoAtividades3 = createButton('3');
+	opcaoAtividades1.addClass('opcaoAtividades');
+	opcaoAtividades2.addClass('opcaoAtividades');
+	opcaoAtividades3.addClass('opcaoAtividades');
+	opcaoAtividades1.position(500,300);
+	opcaoAtividades2.position(600,300);
+	opcaoAtividades3.position(700,300);
+	opcaoAtividades1.mousePressed(iniciadorPuzzleUm);
+	opcaoAtividades2.mousePressed(iniciadorPuzzleDois);
+
+}
+function iniciadorPuzzleUm(){
+	iniciaPuzzleUm();
+	controladorPuzzle = 1;
+}
+function iniciadorPuzzleDois(){
+	iniciaPuzzleDois();
+	controladorPuzzle = 2;
+}
+function quadroPuzzles(puzzle){
+	switch(puzzle){
+		case 0:
+		break;
+		case 1:
+			desenhadorPuzzleUm();
+		break;
+		case 2:
+			desenhadorPuzzleDois();
+		break;
+	}
+}
---
+++ js/puzzle2.js
@@ -0,0 +1,120 @@
+var imgBalaoA, imgBalaoB, imgBalaoC, imgBalaoD, imgBalaoE;
+var vetorSpriteBaloes;
+var corAtual;
+var ponteiro; //variavel pra armazenar o objeto do cursor
+var baloes;
+var estado=1;
+var tempoBalao=0; 
+var baloesPegos=0;
+
+
+function entregaImagem(numero){
+	switch(numero){
+		case 0:
+			return imgBalaoA;
+		break;
+		case 1:
+			return imgBalaoB;
+		break;
+		case 2:
+			return imgBalaoC;
+		break;
+		case 3:
+			return imgBalaoD;
+		break;
+		default:
+			return imgBalaoE;
+		break;
+	}
+}
+function entregaPalavra(numero){
+	switch(numero){
+		case 0:
+			return "blua";
+		break;
+		case 1:
+			return "verda";
+		break;
+		case 2:
+			return "ruga";
+		break;
+		case 3:
+			return "flava";
+		break;
+		default:
+			return "nigra";
+		break;
+	}
+}
+function criaBalao(){
+	let balao = createSprite(random(400,1000),800);
+	balao.tipo = Math.floor(random(4));
+	balao.addAnimation("padrao",entregaImagem(balao.tipo));
+	balao.velocity.y = random(-2,-2.7);
+	balao.velocity.x = random(-0.5,0.5);
+	balao.lifespan = 1000;
+	balao.depth = -1000;
+	balao.onMousePressed = function(){
+		if(balao.tipo==corAtual){
+			balao.remove();
+			corAtual=Math.floor(random(4));
+			baloesPegos++;
+		}
+	}
+	return balao;
+}
+function incrementaTempo(){
+	if(tempoBalao>=45){
+		baloes.add(criaBalao());
+		tempoBalao=0;
+	}
+	tempoBalao++;
+}
+
+function movimentaCursor(){
+	ponteiro.position.x = mouseX; //atualiza o cursor
+	ponteiro.position.y = mouseY;
+	if(mouseIsPressed){
+		ponteiro.changeAnimation("segurando");
+	}
+	else{
+		ponteiro.changeAnimation("padrao");
+	}
+}
+
+function desenhadorPuzzleDois(){
+	switch(estado){
+		case 1:
+			clear();
+			movimentaCursor();
+			incrementaTempo();
+			drawSprites();
+			text(entregaPalavra(corAtual),100,100);
+			text("Balões Pegos 10/ " + baloesPegos,100,200);
+		break;
+		case 2:
+			clear();
+			textSize(70);
+			cursor();
+			text("GRATUJN!",width/2,height/2);
+		break;
+	}
+	if(baloesPegos==10){
+		estado=2;
+	}
+}
+function iniciaPuzzleDois(){
+	removeElements();
+	baloes = new Group();
+	noCursor();
+	textSize(30);
+	vetorSpriteBaloes = [];
+	for(let i=0; i<5; i++){
+		baloes.add(criaBalao());
+	}
+	corAtual = Math.floor(random(4));
+	ponteiro = createSprite(mouseX,mouseY); //criacao do objeto do cursor
+	ponteiro.addAnimation("padrao","imagens/cursor1.png");
+	ponteiro.addAnimation("segurando","imagens/cursor2.png");
+	ponteiro.depth = 1000;
+}
